b0fde854621f13f2673eb0d29d9d03e0406a0072
YourFigo <501385003@qq.com>
2019-11-09 19:08:10 +0800

    mybatis 基于注解 单表操作

--- mybatis_annotation/src/main/java/cn/figo/dao/IUserDao.java
+++ mybatis_annotation/src/main/java/cn/figo/dao/IUserDao.java
@@ -1,7 +1,10 @@
 package cn.figo.dao;
 
 import cn.figo.domain.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -17,4 +20,50 @@ public interface IUserDao {
      */
     @Select("select * from user")
     List<User> findAll();
+
+    /**
+     * 保存用户
+     * @param user
+     */
+    @Insert("insert into user(username,address,sex,birthday)values(#{username},#{address},#{sex},#{birthday})")
+    void saveUser(User user);
+
+    /**
+     * 更新用户
+     * @param user
+     */
+    @Update("update user set username=#{username},sex=#{sex},birthday=#{birthday},address=#{address} where id=#{id}")
+    void updateUser(User user);
+
+    /**
+     * 删除用户
+     * @param userId
+     */
+    @Delete("delete from user where id=#{id} ")
+    void deleteUser(Integer userId);
+
+    /**
+     * 根据id查询用户
+     * @param userId
+     * @return
+     */
+    @Select("select * from user  where id=#{id} ")
+    User findById(Integer userId);
+
+    /**
+     * 根据用户名称模糊查询
+     * @param username
+     * @return
+     */
+    @Select("select * from user where username like #{username} ")
+//    @Select("select * from user where username like '%${value}%' ")
+    List<User> findUserByName(String username);
+
+    /**
+     * 查询总用户数量
+     * @return
+     */
+    @Select("select count(*) from user ")
+    int findTotalUser();
+
 }
--- mybatis_annotation/src/test/java/cn/figo/AnnotationCRUDTest.java
+++ mybatis_annotation/src/test/java/cn/figo/AnnotationCRUDTest.java
@@ -1,6 +1,7 @@
 package cn.figo;
 
 import cn.figo.dao.IUserDao;
+import cn.figo.domain.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,6 +11,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @Author Figo
@@ -23,7 +26,7 @@ public class AnnotationCRUDTest {
     private IUserDao userDao;
 
     @Before
-    public  void init()throws Exception{
+    public  void init() throws Exception{
         in = Resources.getResourceAsStream("SqlMapConfig.xml");
         factory = new SqlSessionFactoryBuilder().build(in);
         session = factory.openSession();
@@ -31,7 +34,7 @@ public class AnnotationCRUDTest {
     }
 
     @After
-    public  void destroy()throws  Exception{
+    public  void destroy() throws  Exception{
         session.commit();
         session.close();
         in.close();
@@ -40,6 +43,48 @@ public class AnnotationCRUDTest {
 
     @Test
     public void testSave(){
+        User user = new User();
+        user.setUsername("mybatis annotation");
+        user.setAddress("北京市昌平区");
 
+        userDao.saveUser(user);
+    }
+
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        user.setId(53);
+        user.setUsername("mybatis annotation update");
+        user.setAddress("北京市海淀区");
+        user.setSex("男");
+        user.setBirthday(new Date());
+
+        userDao.updateUser(user);
+    }
+
+    @Test
+    public void testDelete(){
+        userDao.deleteUser(51);
+    }
+
+    @Test
+    public void testFindOne(){
+        User user = userDao.findById(53);
+        System.out.println(user);
+    }
+
+    @Test
+    public  void testFindByName(){
+        List<User> users = userDao.findUserByName("%mybatis%");
+//        List<User> users = userDao.findUserByName("mybatis");
+        for(User user : users){
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public  void testFindTotal(){
+        int total = userDao.findTotalUser();
+        System.out.println(total);
     }
 }
