f77a41baffafafbb4799c250b00f1ebacd3910c6
YourFigo <501385003@qq.com>
2019-11-19 23:32:12 +0800

    spring ioc 基于xml的一个account案例，xml注入和注解注入

--- spring_ioc_3_account_xml/src/main/java/cn/figo/domain/Account.java
+++ spring_ioc_3_account_xml/src/main/java/cn/figo/domain/Account.java
@@ -4,5 +4,49 @@ package cn.figo.domain;
  * @Author Figo
  * @Date 2019/11/19 21:00
  */
-public class Account {
+
+import java.io.Serializable;
+
+/**
+ * 账户的实体类
+ */
+public class Account implements Serializable {
+
+    private Integer id;
+    private String name;
+    private Float money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
 }
+
