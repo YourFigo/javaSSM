3582ae878dd99b17859493551456b7b36b52ed07
YourFigo <501385003@qq.com>
2019-11-20 21:52:13 +0800

    spring aop 未实现事务控制的转账案例

---
+++ spring_aop_1_transaction/src/main/java/cn/figo/dao/IAccountDao.java
@@ -0,0 +1,47 @@
+package cn.figo.dao;
+
+/**
+ * @Author Figo
+ * @Date 2019/11/19 21:00
+ */
+import cn.figo.domain.Account;
+
+import java.util.List;
+
+/**
+ * 账户的持久层接口
+ */
+public interface IAccountDao {
+
+    /**
+     * 查询所有
+     * @return
+     */
+    List<Account> findAllAccount();
+
+    /**
+     * 查询一个
+     * @return
+     */
+    Account findAccountById(Integer accountId);
+
+    /**
+     * 保存
+     * @param account
+     */
+    void saveAccount(Account account);
+
+    /**
+     * 更新
+     * @param account
+     */
+    void updateAccount(Account account);
+
+    /**
+     * 删除
+     * @param acccountId
+     */
+    void deleteAccount(Integer acccountId);
+
+    Account findAccountByName(String accountName);
+}
