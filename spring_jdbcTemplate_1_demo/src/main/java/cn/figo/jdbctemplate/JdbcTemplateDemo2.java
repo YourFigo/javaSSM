4ab76d7696eccaec7f28ce3f0b3da9d64e255eb1
YourFigo <501385003@qq.com>
2019-11-28 22:35:06 +0800

    spring jdbcTemplate 基本使用

---
+++ spring_jdbcTemplate_1_demo/src/main/java/cn/figo/jdbctemplate/JdbcTemplateDemo2.java
@@ -0,0 +1,36 @@
+package cn.figo.jdbctemplate;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ * JdbcTemplate的最基本用法
+ */
+public class JdbcTemplateDemo2 {
+
+    public static void main(String[] args) {
+        //1.获取容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        //2.获取对象
+        JdbcTemplate jt = ac.getBean("jdbcTemplate",JdbcTemplate.class);
+        //3.执行操作
+        jt.execute("insert into account(name,money)values('ddd',2222)");
+
+
+     /*   //准备数据源：spring的内置数据源
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/eesy");
+        ds.setUsername("root");
+        ds.setPassword("1234");
+
+        //1.创建JdbcTemplate对象
+        JdbcTemplate jt = new JdbcTemplate();
+        //给jt设置数据源
+        jt.setDataSource(ds);
+        //2.执行操作
+        jt.execute("insert into account(name,money)values('ccc',1000)");*/
+    }
+}
---
+++ spring_jdbcTemplate_1_demo/src/main/java/cn/figo/jdbctemplate/JdbcTemplateDemo3.java
@@ -0,0 +1,26 @@
+package cn.figo.jdbctemplate;
+
+import cn.figo.dao.IAccountDao;
+import cn.figo.domain.Account;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * JdbcTemplate的最基本用法
+ */
+public class JdbcTemplateDemo3 {
+
+    public static void main(String[] args) {
+        //1.获取容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        //2.获取对象
+        IAccountDao accountDao = ac.getBean("accountDao",IAccountDao.class);
+
+        Account account = accountDao.findAccountById(1);
+        System.out.println(account);
+
+        account.setMoney(30000f);
+        accountDao.updateAccount(account);
+    }
+}
