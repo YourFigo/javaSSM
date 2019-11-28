b8e4467c65ac29dcc2303bf39f4c0833c1c050eb
YourFigo <501385003@qq.com>
2019-11-29 00:02:42 +0800

    spring-tx 进行事务控制  xml配置

---
+++ spring_transaction_1_xml/src/main/java/cn/figo/dao/impl/AccountDaoImpl.java
@@ -0,0 +1,38 @@
+package cn.figo.dao.impl;
+
+
+import cn.figo.dao.IAccountDao;
+import cn.figo.domain.Account;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+import java.util.List;
+
+/**
+ * 账户的持久层实现类
+ */
+public class AccountDaoImpl extends JdbcDaoSupport implements IAccountDao {
+
+    @Override
+    public Account findAccountById(Integer accountId) {
+        List<Account> accounts = super.getJdbcTemplate().query("select * from account where id = ?",new BeanPropertyRowMapper<Account>(Account.class),accountId);
+        return accounts.isEmpty()?null:accounts.get(0);
+    }
+
+    @Override
+    public Account findAccountByName(String accountName) {
+        List<Account> accounts = super.getJdbcTemplate().query("select * from account where name = ?",new BeanPropertyRowMapper<Account>(Account.class),accountName);
+        if(accounts.isEmpty()){
+            return null;
+        }
+        if(accounts.size()>1){
+            throw new RuntimeException("结果集不唯一");
+        }
+        return accounts.get(0);
+    }
+
+    @Override
+    public void updateAccount(Account account) {
+        super.getJdbcTemplate().update("update account set name=?,money=? where id=?",account.getName(),account.getMoney(),account.getId());
+    }
+}
