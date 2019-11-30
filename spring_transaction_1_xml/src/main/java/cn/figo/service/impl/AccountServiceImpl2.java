344a94ac9cc62365a0202fb720dc07ba478d84da
YourFigo <501385003@qq.com>
2019-11-30 16:35:05 +0800

    编程式事务控制

---
+++ spring_transaction_1_xml/src/main/java/cn/figo/service/impl/AccountServiceImpl2.java
@@ -0,0 +1,67 @@
+package cn.figo.service.impl;
+
+import cn.figo.dao.IAccountDao;
+import cn.figo.domain.Account;
+import cn.figo.service.IAccountService;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+/**
+ * 账户的业务层实现类
+ *
+ * 编程式事务控制，不常用，背离了AOP思想
+ */
+public class AccountServiceImpl2 implements IAccountService {
+
+    private IAccountDao accountDao;
+
+    public void setAccountDao(IAccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    private TransactionTemplate transactionTemplate;
+
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
+    }
+
+    @Override
+    public Account findAccountById(Integer accountId) {
+        return  transactionTemplate.execute(new TransactionCallback<Account>() {
+            @Override
+            public Account doInTransaction(TransactionStatus status) {
+                return accountDao.findAccountById(accountId);
+            }
+        });
+    }
+
+
+
+    @Override
+    public void transfer(String sourceName, String targetName, Float money) {
+        transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                System.out.println("transfer..2222....");
+                //2.1根据名称查询转出账户
+                Account source = accountDao.findAccountByName(sourceName);
+                //2.2根据名称查询转入账户
+                Account target = accountDao.findAccountByName(targetName);
+                //2.3转出账户减钱
+                source.setMoney(source.getMoney()-money);
+                //2.4转入账户加钱
+                target.setMoney(target.getMoney()+money);
+                //2.5更新转出账户
+                accountDao.updateAccount(source);
+
+//                int i=1/0;
+
+                //2.6更新转入账户
+                accountDao.updateAccount(target);
+                return null;
+            }
+        });
+
+    }
+}
