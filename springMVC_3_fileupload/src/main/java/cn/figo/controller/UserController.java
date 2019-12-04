e8f524b5ed3cf066c499aab488482cb1e89e932c
YourFigo <501385003@qq.com>
2019-12-04 23:55:43 +0800

    文件上传 三种 完成

--- springMVC_3_fileupload/src/main/java/cn/figo/controller/UserController.java
+++ springMVC_3_fileupload/src/main/java/cn/figo/controller/UserController.java
@@ -1,11 +1,14 @@
 package cn.figo.controller;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -20,6 +23,12 @@ import java.util.UUID;
 @RequestMapping("/user")
 public class UserController {
 
+    /**
+     * 传统文件上传
+     * @param request
+     * @return
+     * @throws Exception
+     */
     @RequestMapping("/fileupload1")
     public String fileUpload1(HttpServletRequest request) throws Exception {
         System.out.println("文件上传");
@@ -39,7 +48,7 @@ public class UserController {
         // 解析request
         List<FileItem> items = upload.parseRequest(request);
         // 遍历
-        for(FileItem item:items){
+        for(FileItem item : items){
             // 进行判断，当前item对象是否是上传文件项
             if(item.isFormField()){
                 // 说明普通表单向
@@ -56,6 +65,70 @@ public class UserController {
                 item.delete();
             }
         }
+        return "success";
+    }
+
+    /**
+     * SpringMVC文件上传
+     * 前端控制器将请求发给文件解析器，文件解析器拿到upload传给控制器
+     * MultipartFile 参数名upload必须和前端input中的name相同
+     * @return
+     */
+    @RequestMapping("/fileupload2")
+    public String fileuoload2(HttpServletRequest request, MultipartFile upload) throws Exception {
+        System.out.println("springmvc文件上传...");
+
+        // 使用fileupload组件完成文件上传
+        // 上传的位置
+        String path = request.getSession().getServletContext().getRealPath("/uploads/");
+        // 判断，该路径是否存在
+        File file = new File(path);
+        if(!file.exists()){
+            // 创建该文件夹
+            file.mkdirs();
+        }
+
+        // 说明上传文件项
+        // 获取上传文件的名称
+        String filename = upload.getOriginalFilename();
+        // 把文件的名称设置唯一值，uuid
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        filename = uuid+"_"+filename;
+        // 完成文件上传
+        upload.transferTo(new File(path,filename));
+
+        return "success";
+    }
+
+    /**
+     * 跨服务器文件上传
+     * 需要在tomcat服务器目录下conf/web.xml中配置，否则报 403
+     *      <init-param>
+     * 			<param-name>readonly</param-name>
+     * 			<param-value>false</param-value>
+     * 		</init-param>
+     * @return
+     */
+    @RequestMapping("/fileupload3")
+    public String fileuoload3(MultipartFile upload) throws Exception {
+        System.out.println("跨服务器文件上传...");
+
+        // 定义上传文件服务器路径
+        String path = "http://localhost:9090/uploads/";
+
+        // 获取上传文件的名称
+        String filename = upload.getOriginalFilename();
+        // 把文件的名称设置唯一值，uuid
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        filename = uuid+"_"+filename;
+
+        // 创建客户端的对象
+        Client client = Client.create();
+        // 和图片服务器进行连接
+        WebResource webResource = client.resource(path + filename);
+        // 上传文件
+        webResource.put(upload.getBytes());
+
         return "success";
     }
 }
---
+++ springMVC_3_fileuploadserver/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/5/4
+  Time: 21:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>upload server</title>
+</head>
+<body>
+
+
+</body>
+</html>
